69b1f80479c96f425eb898b52f4c406a35307d06
Ayan Mullick <ayanmullick@gmail.com>
2021-02-03 20:27:45 -0600

    Update profile.ps1
    Adding predictor

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,15 @@
 #Install-Module AzurePSDrive,SHIPS
-Import-Module AzurePSDrive
-New-PSDrive -Name Azure -PSProvider SHiPS -root 'AzurePSDrive#Azure'
-Set-Location Azure:
+#Import-Module AzurePSDrive
+#New-PSDrive -Name Azure -PSProvider SHiPS -root 'AzurePSDrive#Azure'
+#Set-Location Azure:
+
+Import-Module -Name PSReadLine -Version 2.2.0 -Verbose
+Import-Module Az.Tools.Predictor -Verbose
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
 
 
 
 #Installs Profile--->   Invoke-Expression (Invoke-WebRequest -UseBasicParsing http://bit.ly/ayanProfile)  or  https://git.io/fhGoi
+#                       iex (iwr -UseBasicParsing https://git.io/fhGoi)
