04d2f21466a7b2e2ad7d3281c96c7cdd19b38eae
Ayan Mullick <ayanmullick@gmail.com>
2021-06-04 16:57:05 -0500

    Update LeetCode.ps1

--- LeetCode.ps1
+++ LeetCode.ps1
@@ -1,1 +1,1 @@
-(1..15)|%{$a = ""; if($_%3 -eq 0){$a +='Fizz'};if($_%5 -eq 0){$a+='Buzz'};if($a -eq ""){$a =$_}; $a} #FizBuzz
+(1..15)|%{$a = ""; if($_%3 -eq 0){$a +='Fizz'};if($_%5 -eq 0){$a+='Buzz'};if($a -eq ""){$a =$_}; $a} #412| FizBuzz|https://leetcode.com/problems/fizz-buzz/
