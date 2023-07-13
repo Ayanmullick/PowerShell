23a528be1fc929beace6203b634e4bfd8ef1cfe0
Ayan Mullick <ayanmullick@gmail.com>
2023-07-13 17:07:24 -0500

    Update LeetCode.ps1

--- LeetCode.ps1
+++ LeetCode.ps1
@@ -1,1 +1,3 @@
 (1..15)|%{$a = ""; if($_%3 -eq 0){$a +='Fizz'};if($_%5 -eq 0){$a+='Buzz'};if($a -eq ""){$a =$_}; $a} #412| FizBuzz|https://leetcode.com/problems/fizz-buzz/
+
+1..15|%{@("Fizz")[$_%3]+{Buzz}[$_%5]??$_} #v2: https://youtu.be/j3iuoxmZ6sY?list=PLfeA8kIs7Cof3Ev-KGonHLdFh8joxDFdW&t=3450
