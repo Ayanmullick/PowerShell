18fb8a9f1d68ca7b340067383efadae0e9519295
Ayan <ayanmullick@gmail.com>
2023-12-06 22:11:32 -0600

    Unistall

--- UpdateUninstallModules.ps1
+++ UpdateUninstallModules.ps1
@@ -3,9 +3,10 @@
 #Remove Scope and parallel while running on PS5.1.
 #Ran sucessfully in updating. However, didn't uninstall. Had to run again. Add '-Wait' parameter so it waits till update finishes to uninstall
 $WarningPreference='silentlyContinue'
-Get-InstalledModule | ForEach-Object -Parallel {$Name= $PSItem.Name; $Vpresent=$PSItem.Version; $Vavailable=$(Find-Module $Name).Version                                                                                                     #Get Variables so parallel works fine
-        if($Vavailable -gt $Vpresent) {Write-Host "Updating $Name at $($PSItem.InstalledLocation) from $Vpresent to $Vavailable" -ForegroundColor Magenta; Update-Module -Name $Name -Scope AllUsers -Force}                                 #Update outdated module
-              else {Write-Host "$Name's version $Vpresent is up-to-date." -ForegroundColor Green}
+Get-InstalledModule | ForEach-Object -Parallel {$Name= $PSItem.Name; $Vpresent=$PSItem.Version; $Vavailable=$(Find-Module $Name).Version  #Get Variables so parallel works fine
+      if($Vavailable -gt $Vpresent) {Write-Host "Updating $Name at $($PSItem.InstalledLocation) from $Vpresent to $Vavailable" -ForegroundColor Magenta; Update-Module -Name $Name -Scope AllUsers -Force}   
+            else {Write-Host "$Name's version $Vpresent is up-to-date." -ForegroundColor Green}                                           #Update outdated module
 
-        Get-InstalledModule $Name -AllVersions|Where-Object {$_.Version -lt $Vpresent}|ForEach-Object {Write-Host "- Uninstalling $Name version $($PSItem.Version)..." -ForegroundColor Magenta; $PSItem | Uninstall-Module -Force -Verbose} #Uninstall older versions
-                                               }
+      Get-InstalledModule $Name -AllVersions|Where-Object {$_.Version -lt $Vpresent}|ForEach-Object {Write-Host "- Uninstalling $Name version $($PSItem.Version)..." -ForegroundColor Magenta; $PSItem | 
+            Uninstall-Module -Force -Verbose}                                                                                             #Uninstall older versions
+                                          }
