cbc4cfc9956840b067b24fc797752ea2574b755a
Ayan <ayanmullick@gmail.com>
2024-11-11 17:59:31 -0600

    .

---
+++ PSScriptAnalyzer.ps1
@@ -0,0 +1,11 @@
+﻿Install-Module PSScriptAnalyzer -Verbose   #https://searchitoperations.techtarget.com/tutorial/Try-PSScriptAnalyzer-to-check-PowerShell-code-best-practices
+
+
+Get-ScriptAnalyzerRule | Sort-Object RuleName | Select-Object CommonName
+Get-ScriptAnalyzerRule | ?{$_.CommonName -eq 'Align assignment statement'}|ft -AutoSize
+Invoke-ScriptAnalyzer .\PSSA.ps1
+
+
+
+Get-ScriptAnalyzerRule -Name *alias*
+Get-ScriptAnalyzerRule -Severity Warning
