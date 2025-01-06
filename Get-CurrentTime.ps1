65b63b0ed4b1d8ced55bcc3735a29bf1bc448a94
Ayan <ayanmullick@gmail.com>
2025-01-06 17:21:11 -0600

    .

--- Get-CurrentTime.ps1
+++ Get-CurrentTime.ps1
@@ -5,3 +5,11 @@ process {
     $currentTime = Get-Date
     Write-Output $currentTime
 }
+
+<#Script installation steps
+$Script   = 'Get-CurrentTime' #Replace with Script name
+$DestPath = [Environment]::GetFolderPath('MyDocuments')+"\PowerShell\Scripts"  #User's default script folder
+
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/Ayanmullick/PowerShell/raw/refs/heads/master/$Script.ps1","$DestPath\$Script.ps1")   #Download script
+If (($env:PATH -split ';') -notcontains $DestPath) {$env:Path += ";$DestPath"} #Add Script folder path to environment variable, if not present, for intellisense.
+#>
--- TestModule/TestModule.psm1
+++ TestModule/TestModule.psm1
@@ -7,3 +7,14 @@ function Get-CurrentTime {
         Write-Output $currentTime
     }
 }
+
+
+<# Installation steps. | The manifest isn't included. 
+GitHub doesn't natively support downloading folders. One needs to download the files individually or use sparse checkout to clone one folder.
+
+$Module = 'TestModule'
+$DestPath = "$([Environment]::GetFolderPath('MyDocuments'))\PowerShell\Modules\$Module"
+New-Item -ItemType Directory -Path $DestPath -Force -ErrorAction SilentlyContinue
+(New-Object Net.WebClient).DownloadFile("https://github.com/Ayanmullick/PowerShell/raw/refs/heads/master/$Module/$Module.psm1", "$DestPath\$Module.psm1")
+
+#>
