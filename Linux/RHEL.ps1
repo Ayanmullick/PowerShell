22240b1e4e51fc07e5d36afac554256e7c659460
Ayan <ayanmullick@gmail.com>
2025-05-15 19:43:18 -0500

    Linux

---
+++ Linux/RHEL.ps1
@@ -0,0 +1,72 @@
+﻿#region Windows to Redhat
+rpm -qia '*release*'  #RedHat version
+
+#https://superuser.com/questions/1296024/windows-ssh-permissions-for-private-key-are-too-open/1296046#1296046
+ssh -i .\ayan.pem ayan@192.168.0.5  #worked
+
+#On RHEL 8: Direct download option dodn't work. https://docs.microsoft.com/en-us/powershell/scripting/install/install-rhel?view=powershell-7.1#installation-via-package-repository
+curl https://packages.microsoft.com/config/rhel/8/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo # Register the Microsoft RedHat repository
+sudo dnf install powershell # Install PowerShell
+pwsh # Start PowerShell
+
+
+#On Windows client
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
+
+#Add subsystem to SSH config
+sudo nano /etc/ssh/sshd_config 
+Subsystem powershell /usr/bin/pwsh -sshs -NoLogo -NoProfile #CTRL+O ENTER to save and CTRL+X key to close nano editor
+sudo service sshd restart  
+
+
+#Connect
+ssh -i .\ayan.pem ayan@192.168.0.5   #Over private IP
+ssh -i .\ayan.pem ayan@20.80.38.198  #Over public IP
+
+
+$pssession = New-PSSession -Name RHELSession -HostName 192.168.0.5 -Port 22 -Subsystem powershell -UserName ayan -KeyFilePath .\ayan.pem -SSHTransport -Verbose  #Over private IP
+$pssession = New-PSSession -Name RHELSession -HostName 20.80.38.198 -Port 22 -Subsystem powershell -UserName ayan -KeyFilePath .\ayan.pem -SSHTransport -Verbose  #Over public IP
+Invoke-Command -Session $pssession -ScriptBlock {hostname}
+
+
+#With password. Additional steps needed while connecting with password.
+ssh ayan@192.168.0.6  #works
+
+
+$Cred= New-Object System.Management.Automation.PSCredential "<>",$(ConvertTo-SecureString '<>' -asplaintext -force)
+#Without storing credential on disk
+$Cred= New-Object System.Management.Automation.PSCredential "<>",$(ConvertTo-SecureString "$(Get-AzKeyVaultSecret -VaultName RHEL84 -Name ayanPswd -AsPlainText)" -asplaintext -force)
+New-SSHSession -ComputerName 65.52.236.54 -Port 22 -Credential $Cred -Verbose
+(Invoke-SSHCommand -SessionId 0 -Command {hostname}).Output
+
+#endregion
+#Passwordless cert-based remoting isn't an option cross-platform. https://adamtheautomator.com/winrm-ssl/
+
+
+
+#region Didn't work | Creating PS session using password didn't work. Friedrich Weinmann [friedrich.weinmann@microsoft.com ] confirmed SSH is the way forward with Linux
+New-PSSession -ComputerName 192.168.0.6 -Port 22 -Name RHELSession -Credential $Cred -SessionOption $o  -Verbose 
+#-UseSSL: Needs cert configured on Linux VM 
+#  -Authentication Kerberos : -Kerberos is used when no authentication method and no user name are specified.  Kerberos accepts domain user names, but not local user names.
+
+
+$o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck 
+
+set-item wsman:\localhost\client\TrustedHosts RHELPW -Concatenate -Force
+   
+  set winrm/config/client '@{AllowUnencrypted="true"}'
+  winrm set winrm/config/client/auth '@{Basic="true"}'
+  winrm set winrm/config/service/auth '@{Basic="true"}'
+
+#endregion
+
+
+#Doesn't work
+
+New-SSHSession -ComputerName ayan@20.80.38.198 -Port 22 -KeyString $(Get-AzSshKey -ResourceGroupName RHEL -Name ayan).publicKey -Verbose
+
+New-SSHSession -ComputerName 20.80.38.198 -Port 22 -Credential ayan -KeyString $((Get-AzSshKey -ResourceGroupName RHEL -Name ayan).publicKey) -AcceptKey -Verbose
+
+New-SSHSession -ComputerName 20.80.38.198 -Port 22 -Credential ayan -KeyFile .\ayan.pem -AcceptKey -Verbose
+
+New-SSHSession -ComputerName 20.80.38.198 -Port 22 -KeyFile .\ayan.pem -AcceptKey -Verbose
---
+++ Linux/Ubuntu.ps1
@@ -0,0 +1,80 @@
+﻿#Installs Task manager
+sudo apt-get update
+sudo apt-get install gnome-system-monitor
+gnome-system-monitor
+
+
+
+#region Ubuntu 10.04
+lsb_release -a  #check version
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Update the list of products
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+# Start PowerShell
+pwsh
+
+
+install-module SSHSessions
+New-SshSession -ComputerName 52.179.171.150 -Username <> -Password <> -Verbose
+Enter-SSHSshSession -ComputerName 137.116.36.177
+
+#Worked for a VMSS VM with no Entra login enabled
+Enter-AzVM -Ip 172.25.22.11 -LocalUser azureuser -PublicKeyFile ./id_rsa.pub
+#endregion
+
+
+
+touch test.txt #Creates file
+
+
+#Install Snap store https://snapcraft.io/docs/installing-snap-on-ubuntu
+sudo apt update
+sudo apt install snapd  #sudo apt-get install snapd
+
+#https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.2#installation-via-snap
+
+ap2admin@Ubuntu-test:~$ sudo snap install powershell   
+#error: This revision of snap "powershell" was published using classic confinement and thus may perform arbitrary system changes outside of the security sandbox that snaps are usually confined to, which may put your system at risk.
+#If you understand and want to proceed repeat the command including --classic.
+
+sudo snap install powershell --classic  #Worked
+
+sudo snap refresh powershell #This is to update PowerShell once it is installed from the Snap store
+
+#VSCode install | https://code.visualstudio.com/docs/setup/linux#_installation
+sudo apt install ./code_1.66.2-1649664567_amd64.deb
+
+
+#Login as root:  https://linuxize.com/post/how-to-change-root-password-in-ubuntu/
+
+
+#region Docker file to enable xRDP[Opensource RDP implementation] to create the docker image| https://www.youtube.com/watch?v=0rl5145aEMk
+FROM ubuntu:latest
+
+RUN apt update && DEBIAN_FRONTEND=noninteractive apt install -y ubuntu-desktop
+
+RUN rm /run/reboot-required*
+
+RUN useradd -m testuser -p $(openssl passwd 1234)
+RUN usermod -aG sudo testuser
+
+RUN apt install -y xrdp
+RUN adduser xrdp ssl-cert
+
+RUN sed -i '3 a echo "\
+export GNOME_SHELL_SESSION_MODE=ubuntu\\n\
+export XDG_SESSION_TYPE=x11\\n\
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME\\n\
+export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg\\n\
+" > ~/.xsessionrc' /etc/xrdp/startwm.sh
+
+EXPOSE 3389
+
+CMD service dbus start; /usr/lib/systemd/systemd-logind & service xrdp start ; bash
+
+#endregion
