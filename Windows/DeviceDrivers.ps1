2d2219976a720567f8d202eb3c0a7a89f7243fc5
Ayan <ayanmullick@gmail.com>
2025-07-28 21:16:00 -0500

    .

--- Windows/DeviceDrivers.ps1
+++ Windows/DeviceDrivers.ps1
@@ -17,3 +17,39 @@ gwmi win32_systemdriver | select *, @{ N='CompanyName';E={ (Get-ItemProperty $_.
 #3rd party software
 gwmi win32_product|? {$_.name -notlike "*Microsoft*"}|   select name
 gwmi win32_product|? {$_.name -notlike "*Microsoft*" -and $_.vendor -notlike "*Microsoft*" }|select name, version,installdate,vendor|sort installdate -Debug|ft -AutoSize -Wrap
+
+
+Get-PnpDevice -class Bluetooth -FriendlyName 'WF-1000XM4'|fl
+<#Caption                     : WF-1000XM4
+Description                 : Bluetooth Device
+InstallDate                 : 
+Name                        : WF-1000XM4
+Status                      : OK
+Availability                : 
+ConfigManagerErrorCode      : CM_PROB_NONE
+ConfigManagerUserConfig     : False
+CreationClassName           : Win32_PnPEntity
+DeviceID                    : BTHENUM\DEV_AC800AD8F209\7&37DA03EC&0&BLUETOOTHDEVICE_AC800AD8F209
+ErrorCleared                : 
+ErrorDescription            : 
+LastErrorCode               : 
+PNPDeviceID                 : BTHENUM\DEV_AC800AD8F209\7&37DA03EC&0&BLUETOOTHDEVICE_AC800AD8F209
+PowerManagementCapabilities : 
+PowerManagementSupported    : 
+StatusInfo                  : 
+SystemCreationClassName     : Win32_ComputerSystem
+SystemName                  : SRFCL3-W11-1022
+ClassGuid                   : {e0cbf06c-cd8b-4647-bb8a-263b43f0f974}
+CompatibleID                : {BTHENUM\GENERIC_DEVICE}
+HardwareID                  : {BTHENUM\Dev_AC800AD8F209}
+Manufacturer                : Microsoft
+PNPClass                    : Bluetooth
+Present                     : True
+Service                     : 
+PSComputerName              : 
+Class                       : Bluetooth
+FriendlyName                : WF-1000XM4
+InstanceId                  : BTHENUM\DEV_AC800AD8F209\7&37DA03EC&0&BLUETOOTHDEVICE_AC800AD8F209
+Problem                     : CM_PROB_NONE
+ProblemDescription          : 
+#>
