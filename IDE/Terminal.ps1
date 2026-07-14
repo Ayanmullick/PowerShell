8e44994aee9bfbfadd64d72ed8076b7cd2caa06e
Ayan <ayanmullick@gmail.com>
2026-07-14 10:32:12 -0500

    .

--- IDE/Terminal.ps1
+++ IDE/Terminal.ps1
@@ -1,2 +1,8 @@
 #POWERSHELL_UPDATECHECK environment variable
 :SetEnvironmentVariable("POWERSHELL_UPDATECHECK", "LTS", "User")
+<#
+$env:POWERSHELL_UPDATECHECK = "LTS"
+PS C:\Temp> [Environment]::SetEnvironmentVariable("POWERSHELL_UPDATECHECK", "LTS", "User")
+PS C:\Temp> [Environment]::GetEnvironmentVariable("POWERSHELL_UPDATECHECK", "User")
+LTS
+#>
