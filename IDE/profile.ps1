e2dfe44e7841e5e257f331e1ee994e3dbda473fa
Ayan <ayanmullick@gmail.com>
2026-07-21 14:10:30 -0500

    profile

--- IDE/profile.ps1
+++ IDE/profile.ps1
@@ -1,16 +1,8 @@
-#Install-Module AzurePSDrive,SHIPS
-#Import-Module AzurePSDrive
-#New-PSDrive -Name Azure -PSProvider SHiPS -root 'AzurePSDrive#Azure'
-#Set-Location Azure:
-
-#Import-Module -Name PSReadLine -MinimumVersion 2.2.6
-#PSReadLine  2.2.6| Az.Tools.Predictor 1.1.0| CompletionPredictor 0.1.1 #All predictors working fine       
-
 Import-Module CompletionPredictor
 Import-Module Az.Tools.Predictor
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
 
+Set-PSReadLineOption -EnableScreenReaderMode:$false
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
 
 
 
