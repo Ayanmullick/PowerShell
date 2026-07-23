eae40fd4ed64a84a049dcd015df89b2f42e43e64
Ayan <ayanmullick@gmail.com>
2026-07-23 08:44:02 -0500

    prompt

--- IDE/Terminal.ps1
+++ IDE/Terminal.ps1
@@ -6,3 +6,12 @@ PS C:\Temp> [Environment]::SetEnvironmentVariable("POWERSHELL_UPDATECHECK", "LTS
 PS C:\Temp> [Environment]::GetEnvironmentVariable("POWERSHELL_UPDATECHECK", "User")
 LTS
 #>
+
+
+#region #Sets the prompt to the last foldername in the path
+#-Leaf  Indicates that this cmdlet returns only the last item or container in the path.
+function prompt {     
+    $folderName = Get-Location| Split-Path -Leaf   
+    "$folderName > "
+}
+#endregion
